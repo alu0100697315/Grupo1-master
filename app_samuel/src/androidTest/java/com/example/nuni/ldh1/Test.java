c0948e7fec99350a90cff90a1a0e84ca5a2f6ff6
Samuel Dóniz <alu0100697315@ull.edu.es>
2016-07-01 11:30:05 +0100

    Creados los test

---
+++ app_samuel/src/androidTest/java/com/example/nuni/ldh1/Test.java
@@ -0,0 +1,11 @@
+package com.example.nuni.ldh1;
+import android.test.InstrumentationTestCase;
+
+
+
+public class Test extends InstrumentationTestCase {
+
+    public void ReturnsTrue()throws Exception {
+        assertEquals(Nfc.class, true);
+    }
+}
